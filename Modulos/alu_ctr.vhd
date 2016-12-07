60c40a3f19a78fb9d0e22a046a9bd6d15f02b575
ArthurPereira\Arthur Pereira <arthurpereira__@hotmail.com>
2016-12-07 15:55:00 -0200

    atualizando controle da ula

--- Modulos/alu_ctr.vhd
+++ Modulos/alu_ctr.vhd
@@ -6,7 +6,7 @@ use ieee.numeric_std.all;
 entity alu_ctr is
 	port 
 	(
-		op_alu		: in std_logic_vector(1 downto 0);
+		op_alu		: in std_logic_vector(2 downto 0);
 		funct			: in std_logic_vector(5 downto 0);
 		alu_ctr	   : out std_logic_vector(3 downto 0)
 	);
@@ -16,18 +16,24 @@ architecture alu_ctr_arch of alu_ctr is
 signal functOP : std_logic_vector(3 downto 0);
 begin
 with op_alu select alu_ctr <=
-		"0010" when "00", --lw e sw
-		"0100" when "01", -- beq
+		"0010" when "000", --lw e sw
+		"0100" when "001", -- beq e bne
+		"0111" when "011", --nand
+		"0010" when "100", --addi
+		"0001" when "101", --ori
+		"0110" when "110", --slti
 		functOP when others;
 		
 with funct select functOP <=
 		"0010" when "100000", --add
 		"0100" when "100010", --sub
 		"0000" when "100100", --and
-		"0001" when "100101", --or
+		"0001" when "100101", --or 
 		"1000" when "100111", --nor
 		"1001" when "100110", --xor
 		"0110" when "101010", --slt
+		"1010" when "000000", --sll
+		"0010" when "001000", --jr (tipo r)
 		"0000" when others;
 end architecture;
 		
