700f33c79652648f493cac53e31824127eda1f80
Pietro Bertarini <pietrobcmota@gmail.com>
2016-12-16 15:09:47 -0200

    Concertando o .asm e o .mif que estava dando erro de compilacao

--- teste.asm
+++ teste.asm
@@ -1,9 +1,11 @@
 .data 
-	a: .word 1, 2, 3, 4
+	a: .word 2
+	b: .word 5
+	c: .word 3
+	
 .text
-	li $t1, 0x10010000
-	lw $t0, 0($t1)
-	lw $t2, 4($t1)
+	lw $t0,a
+	lw $t1,b
 	add $t3, $t1, $t0
 	addi $s0, $t1, 3
 	sub $t3, $t1, $t0
@@ -12,7 +14,7 @@
 	ori $t7, $t0, 2
 	xor $t0, $t0, $t1
 	nor $t3, $t1, $zero
-	sw $t3, 12($t1)
+	sw $t3, c
 	bne $t1, $t1, Label
 	beq $t1,$t1, Label
 	
